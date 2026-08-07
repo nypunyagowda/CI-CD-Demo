561b3f66ca2c40e6b1c973a4ecffadf9612d7df7
nypunyagowda <nypunyagowda20@gmail.com>
2026-08-07 09:39:39 +0530

    add app and workflow

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
-
+def add_num(a,b):
+    return a+b
+if __name__=="__main__":
+    print(add_num(2,3))
--- test_app.py
+++ test_app.py
@@ -1,1 +1,5 @@
-
+from app import add_num
+def test_add_numbers():
+    assert add_numbers(2,3) == 5
+def test_add_negative():
+    assert add_numbers(-1,1) == 0
